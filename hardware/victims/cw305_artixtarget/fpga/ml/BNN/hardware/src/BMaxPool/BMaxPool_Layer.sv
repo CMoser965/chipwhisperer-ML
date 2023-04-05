d432fcb3cb23e707916283e6440d991cd087f033
Christian Moser <christianwmoser@proton.me>
2023-04-05 01:41:33 -0500

    Unfuck the pooling layer

--- hardware/victims/cw305_artixtarget/fpga/ml/BNN/hardware/src/BMaxPool/BMaxPool_Layer.sv
+++ hardware/victims/cw305_artixtarget/fpga/ml/BNN/hardware/src/BMaxPool/BMaxPool_Layer.sv
@@ -1,7 +1,8 @@
 module BMaxPool #(
     parameter I_SIZE = 26,
     parameter O_SIZE = 13,
-    parameter POOL_SIZE = 4
+    parameter POOL_H = 2,
+    parameter POOL_W = 2
 ) (
     input  [I_SIZE-1:0][I_SIZE-1:0] layer_i,
     input  clk,
@@ -13,9 +14,16 @@ wire  [I_SIZE*I_SIZE-1:0] in_vector = layer_i;
 logic [O_SIZE*O_SIZE-1:0] out_vector;
 
 integer i = 0;
-always @(posedge clk) begin
-    out_vector[i] <= in_vector[i*O_SIZE +: 2] | in_vector[i*O_SIZE+I_SIZE +: 2];
-    i <= i + 2;
+// always @(posedge clk) begin
+
+// end
+
+always @(*) begin
+
+    for(i = 0; i < O_SIZE * O_SIZE; i = i + 1) begin
+        out_vector[i] <= |in_vector[i*POOL_H +: POOL_W] | |in_vector[i*POOL_W+I_SIZE +: POOL_H];
+    end
+
 end
 
 assign layer_o = out_vector;
