f5d313147dda8efd7e6a4aa33fcecf680958e5ee
Christian Moser <christianwmoser@proton.me>
2023-04-23 19:29:34 -0500

    Thingy work now

--- hardware/victims/cw305_artixtarget/fpga/ml/BNN/hardware/signal_proc/decoder.sv
+++ hardware/victims/cw305_artixtarget/fpga/ml/BNN/hardware/signal_proc/decoder.sv
@@ -1,24 +1,25 @@
 module image_stream_decoder (
-    input   [7:0]       slice_i,
-    input   [2:0]       sel,
-    input               en,
-    input               clk,
-    input               rst,
-    output  [7:0][7:0]  image_o
+    input       [7:0]       slice_i,
+    input logic [2:0]       sel,
+    input                   en,
+    input                   clk,
+    input                   rst,
+    output reg [7:0][7:0]  image_o
 );
 
 reg [7:0] slice_sel;
 
-always begin : stream_selection
+always_comb begin
     case(sel) 
-        3'b000  : image_o[0] = slice_sel;
-        3'b001  : image_o[1] = slice_sel;
-        3'b010  : image_o[2] = slice_sel;
-        3'b011  : image_o[3] = slice_sel;
-        3'b100  : image_o[4] = slice_sel;
-        3'b101  : image_o[5] = slice_sel;
-        3'b110  : image_o[6] = slice_sel;
-        3'b101  : image_o[7] = slice_sel;
+        3'b000  : image_o[0][7:0] = slice_sel;
+        3'b001  : image_o[1][7:0] = slice_sel;
+        3'b010  : image_o[2][7:0] = slice_sel;
+        3'b011  : image_o[3][7:0] = slice_sel;
+        3'b100  : image_o[4][7:0] = slice_sel;
+        3'b101  : image_o[5][7:0] = slice_sel;
+        3'b110  : image_o[6][7:0] = slice_sel;
+        3'b111  : image_o[7][7:0] = slice_sel;
+        default : break;
     endcase
 end
 
@@ -27,13 +28,13 @@ flopenr #(8) reg0 (clk, rst, en, slice_i, slice_sel);
 endmodule
 
 module flopenr #(parameter WIDTH = 8) (
-    input logic clk, reset, en,
-    input logic [WIDTH-1:0] d,
-    input logic[WIDTH-1:0] q
+    input  logic clk, reset, en,
+    input  logic [WIDTH-1:0] d,
+    output logic [WIDTH-1:0] q
 );
 
 always_ff @(posedge clk) begin
-    if (reset) q <= WIDTH'b0;
+    if (reset) q  <= 0;
     else if(en) q <= d;
 end
 
--- hardware/victims/cw305_artixtarget/fpga/ml/BNN/hardware/signal_proc/decoder_tb.sv
+++ hardware/victims/cw305_artixtarget/fpga/ml/BNN/hardware/signal_proc/decoder_tb.sv
@@ -1,5 +1,7 @@
 module stimulus ();
+    reg [7:0][7:0] image_construction;
     logic [7:0][7:0] image_test = {
+        8'hFF,
         8'hFF,
         8'hFF,
         8'hFF,
@@ -17,13 +19,13 @@ module stimulus ();
 
     reg[7:0] selection_vector = 8'b0;
 
-    decoder DUT (
+    image_stream_decoder DUT (
         .slice_i(selection_vector),
         .sel(sel),
         .en(en),
         .rst(rst),
         .clk(clk),
-        .image_o(selection_vector)
+        .image_o(image_construction)
     );
 
     initial begin
@@ -31,42 +33,42 @@ module stimulus ();
         #0  rst = 1'b1;
         
         // slice 1
-        #15 rst = 1'b0;
+        #10 rst = 1'b0;
         #0 en = 1'b1;
         #0 selection_vector = image_test[0];
         #0 sel = 3'b000;
 
         // slice 2
-        #5 selection_vector = image_test[1];
+        #10 selection_vector = image_test[1];
         #0 sel = 3'b001;
 
         // slice 3
-        #5 selection_vector = image_test[2];
+        #10 selection_vector = image_test[2];
         #0 sel = 3'b010;
 
         // slice 4
-        #5 selection_vector = image_test[3];
+        #10 selection_vector = image_test[3];
         #0 sel = 3'b011;
 
         // slice 5
-        #5 selection_vector = image_test[4];
+        #10 selection_vector = image_test[4];
         #0 sel = 3'b100;
 
         // slice 6
-        #5 selection_vector = image_test[5];
+        #10 selection_vector = image_test[5];
         #0 sel = 3'b101;
 
         // slice 7
-        #5 selection_vector = image_test[6];
+        #10 selection_vector = image_test[6];
         #0 sel = 3'b110;
 
         // slice 8
-        #5 selection_vector = image_test[7];
-        #0 sel = 3'b101;
+        #10 selection_vector = image_test[7];
+        #0 sel = 3'b111;
 
-        // slice 2
-        #5 selection_vector = image_test[0];
-        #0 sel = 3'b000;
+        #10 $display("Output image construction:");
+        #10 $display(image_construction);
+        // $finish();
     end
 
 endmodule
